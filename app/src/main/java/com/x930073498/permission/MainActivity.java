132448ba3808badf84ab0e8b3b75e0399326890f
x930073498 <x930073498@gmail.com>
2018-10-15 09:54:39 +0800

    update from YJ

--- app/src/main/java/com/x930073498/permission/MainActivity.java
+++ app/src/main/java/com/x930073498/permission/MainActivity.java
@@ -4,9 +4,12 @@ import android.Manifest;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-import com.x930073498.annotations.*;
 import com.x930073498.R;
+import com.x930073498.annotations.NeedPermission;
+import com.x930073498.annotations.PermissionsDenied;
+import com.x930073498.annotations.PermissionsGranted;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
+    }
+
+    @PermissionsGranted(permission = Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    public void onGranted() {
+        Toast.makeText(getApplicationContext(), "允许", Toast.LENGTH_SHORT).show();
+    }
+
+    @PermissionsDenied(permission = Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    public void onDenied() {
+        Toast.makeText(getApplicationContext(), "拒绝", Toast.LENGTH_SHORT).show();
     }
 }
